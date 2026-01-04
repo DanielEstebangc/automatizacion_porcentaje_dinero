a4b947d2fb7f636910b76bbc296c923fe258dec3
Daniel Galvis <danielgalvis.3107@gmail.com>
2026-01-03 23:27:18 -0500

    Render compatible: 0.0.0.0 y puerto dinámico

--- app.py
+++ app.py
@@ -36,5 +36,11 @@ def calcular():
 
     return jsonify(resultado)
 
+# Ejecutar la aplicación en test
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# Ejecutar la aplicación en producción
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render pasa el puerto por env
+    app.run(host="0.0.0.0", port=port)

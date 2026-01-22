8fded777618764aaaaff300de8a3b0045609cfbc
Daniel Galvis <danielgalvis.3107@gmail.com>
2026-01-22 18:02:53 -0500

    cambio en el app para poder montar a produccion

--- app.py
+++ app.py
@@ -36,11 +36,12 @@ def calcular():
 
     return jsonify(resultado)
 
-# Ejecutar la aplicación en test
-if __name__ == "__main__":
-    app.run(debug=True)
+# # Ejecutar la aplicación en test
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 # Ejecutar la aplicación en producción
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # Render pasa el puerto por env
-#     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render pasa el puerto por env
+    app.run(host="0.0.0.0", port=port)
